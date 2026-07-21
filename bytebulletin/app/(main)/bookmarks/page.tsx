6278205313f8cb86beea6e3c81bfea3ee115a469
Mardy <mardytechnologyltd@gmail.com>
2026-07-22 04:55:57 +0600

    Fix typescript error in bookmarks

--- bytebulletin/app/(main)/bookmarks/page.tsx
+++ bytebulletin/app/(main)/bookmarks/page.tsx
@@ -28,7 +28,7 @@ export default async function BookmarksPage() {
 
       {bookmarkedArticles.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-          {bookmarkedArticles.map((article) => (
+          {bookmarkedArticles.map((article: any) => (
             <ArticleCard
               key={article.id}
               id={article.id}
